34254439ae26c54fee6c3868ba21c0046a9ab031
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-08 05:08:51 +0700

    search page done

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
-function Avatar({ url }) {
+function Avatar({ url, className }) {
   return (
     <div>
       <img
-        className="rounded-full h-10
+        className={`rounded-full h-10
         cursor-pointer trasition duration-150
-        transform hover:scale-110"
+        transform hover:scale-110 ${className}`}
         loading="lazy"
         src={url}
         alt="profile"
---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developer.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyAe7Cs6eojh_xBoZxmsG-dldQm5rQmiw9I";
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "0dd429727bc6baf89";
